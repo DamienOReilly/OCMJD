58a433818d61f9ff1d29655d6bf8da2db2c0088e
Damien O'Reilly <damienreilly@gmail.com>
2014-03-07 10:30:11 +0000

    Project code started.

---
+++ src/suncertify/db/DBSchema.java
@@ -0,0 +1,10 @@
+package suncertify.db;
+
+/**
+ * @author Damien O'Reilly
+ */
+public class DBSchema {
+
+    /* Magic to identify a complatible database with this application */
+    public static final int DATABASE_MAGIC = 0x202;
+}
