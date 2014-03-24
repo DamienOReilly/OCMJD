9daa6a7082467c322733fe42ee3979973172eebc
Damien O'Reilly <damienreilly@gmail.com>
2014-03-24 17:02:54 +0000

    Started work on the database IO part.

---
+++ src/main/java/suncertify/db/io/Utils.java
@@ -0,0 +1,27 @@
+package suncertify.db.io;
+
+import suncertify.db.DatabaseSchema;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * @author Damien O'Reilly
+ */
+public class Utils {
+    /**
+     * Reads a string from bytes accessed via {@link java.io.RandomAccessFile}
+     *
+     * @param database
+     *         Database
+     * @param length
+     *         Length of String to read
+     * @return String extracted.
+     * @throws IOException
+     */
+    public static String readBytesAsString(RandomAccessFile database, int length) throws IOException {
+        byte[] input = new byte[length];
+        database.read(input);
+        return new String(input, DatabaseSchema.CHARSET_ENCODING).trim();
+    }
+}
